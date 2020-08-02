b07b470cd15975e2568717cac61e9d642694083d
Niklas Reisser <niklas.reisser@eroad.com>
2020-08-03 04:16:41 +1200

    DASH-691: Ignore PUT errors. Might be simply already there.

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash -e
 
+# Ignore errors when publishing to nexus, 
+# in case one the two files exists already there
+
 cd aws-crt-java/android
 ./gradlew publishToMavenLocal
-./gradlew publishReleasePublicationToMavenRepository
+./gradlew publishReleasePublicationToMavenRepository || true
 cd ../aws-iot-device-sdk-java-v2/android
 ./gradlew publishToMavenLocal
-./gradlew publishReleasePublicationToMavenRepository
+./gradlew publishReleasePublicationToMavenRepository || true
