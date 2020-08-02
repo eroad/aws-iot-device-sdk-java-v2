1e640af0ea63291570ae1cd2819e6aea7ca2266b
Niklas Reisser <niklas.reisser@eroad.com>
2020-08-03 04:00:03 +1200

    DASH-691: Typo in gradle command

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -2,7 +2,7 @@
 
 cd aws-crt-java/android
 ./gradlew publishToMavenLocal
-./gradlew publishReleasePublicationsToMavenRepository
+./gradlew publishReleasePublicationToMavenRepository
 cd ../aws-iot-device-sdk-java-v2/android
 ./gradlew publishToMavenLocal
-./gradlew publishReleasePublicationsToMavenRepository
+./gradlew publishReleasePublicationToMavenRepository
